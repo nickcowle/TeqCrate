fb5e3314e0b951e7dbf3a30275d0cdde2f33007e
Nicholas Cowle <nickcowle@gmail.com>
2019-04-06 12:01:41 +0100

    Fix Reflection.invokeStaticMethod to work on functions with more than one argument

--- TeqCrate/Reflection.fs
+++ TeqCrate/Reflection.fs
@@ -9,9 +9,14 @@ module Reflection =
 
     let invokeStaticMethod (e : Expr) (ts : Type seq) (vs : obj seq) : obj =
 
-        match e with
-        | Lambda (_, Call (_, mi, _)) -> mi
-        | _ -> failwith "Could not get MethodInfo"
+        let rec getMethodInfo =
+            function
+            | Call (_, mi, _) -> mi
+            | Lambda (_, e) -> getMethodInfo e
+            | _ -> failwith "Could not get MethodInfo"
+
+        e
+        |> getMethodInfo
         |> fun mi -> mi.GetGenericMethodDefinition ()
         |> fun mi -> mi.MakeGenericMethod (ts |> Array.ofSeq)
         |> fun mi -> mi.Invoke(null, vs |> Array.ofSeq)
