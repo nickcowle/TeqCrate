c386e84f618da8290a5180d7e7205e178800053f
Nicholas Cowle <nickcowle@gmail.com>
2019-09-22 18:30:52 +0100

    Add examples from Type-Safe Datatype-Generic Programming in F# talk

--- Examples/CsvParser.fsx
+++ Examples/CsvParser.fsx
@@ -1,3 +1,5 @@
+// Note: run 'dotnet publish' in the root of this repo to ensure that the dlls referenced in this script are present
+
 #r "netstandard"
 #I @"..\TeqCrate\bin\Debug\netstandard2.0\publish"
 #r "TypeEquality.dll"
@@ -11,41 +13,35 @@ open TeqCrate
 open TeqCrate.Patterns
 open TypeEquality
 
-let parseCell<'a> (cell : string) : 'a =
+let parseCell<'a> : string -> 'a =
     match tType<'a> with
-    | String   teq -> cell |> Teq.castFrom teq
-    | Bool     teq -> cell |> Boolean.Parse   |> Teq.castFrom teq
-    | Int      teq -> cell |> Int32.Parse     |> Teq.castFrom teq
-    | Float    teq -> cell |> Double.Parse    |> Teq.castFrom teq
-    | DateTime teq -> cell |> DateTime.Parse  |> Teq.castFrom teq
+    | String   (teq : Teq<'a, string  >) -> Teq.castFrom teq
+    | Bool     (teq : Teq<'a, bool    >) -> Boolean .Parse >> Teq.castFrom teq
+    | Int      (teq : Teq<'a, int     >) -> Int32   .Parse >> Teq.castFrom teq
+    | Float    (teq : Teq<'a, float   >) -> Double  .Parse >> Teq.castFrom teq
+    | DateTime (teq : Teq<'a, DateTime>) -> DateTime.Parse >> Teq.castFrom teq
     | _ -> failwithf "Error - the type %s is not supported" (typeof<'a>.FullName)
 
-let rec makeRowReader<'ts> (ts : 'ts TypeList) (index : int) : string array -> 'ts HList =
+let rec parseRow<'ts> (ts : 'ts TypeList) (cells : string list) : 'ts HList =
     match TypeList.split ts with
-    | Choice1Of2 teq ->
-        let empty = HList.empty |> Teq.castFrom (HList.cong teq)
-        fun _ -> empty
+    | Choice1Of2 (teq : Teq<'ts, unit>) -> HList.empty |> Teq.castFrom (HList.cong teq)
     | Choice2Of2 crate ->
         crate.Apply
             { new TypeListConsEvaluator<_,_> with
-                member __.Eval ts teq =
-                    let rowReader = makeRowReader ts (index + 1)
-                    fun cells ->
-                        cells
-                        |> rowReader
-                        |> HList.cons (cells.[index] |> parseCell)
-                        |> Teq.castFrom (HList.cong teq)
+                member __.Eval (us : 'us TypeList) (teq : Teq<'ts, 'u -> 'us>) =
+                    let head = cells |> List.head |> parseCell<'u>
+                    let tail = cells |> List.tail |> parseRow us
+                    HList.cons head tail |> Teq.castFrom (HList.cong teq)
             }
 
-let tryParse<'record> (filePath : string) : 'record seq option =
+let tryParse<'record> (fileInfo : FileInfo) : 'record seq option =
     match tType<'record> with
     | Record crate ->
         crate.Apply
             { new RecordConvEvaluator<_,_> with
-                member __.Eval _ ts conv =
-                    let rowReader = makeRowReader ts 0
-                    File.ReadLines filePath
-                    |> Seq.map ((fun s -> s.Split ',') >> rowReader >> conv.From)
+                member __.Eval _ (ts : 'ts TypeList) (conv : Conv<'record, 'ts HList>) =
+                    File.ReadLines fileInfo.FullName
+                    |> Seq.map (fun row -> row.Split ',' |> List.ofArray |> parseRow ts |> conv.From)
                     |> Some
             }
     | _ -> None
--- Examples/ExampleParse.fsx
+++ Examples/ExampleParse.fsx
@@ -1,3 +1,5 @@
+// Note: run 'dotnet publish' in the root of this repo to ensure that the dlls referenced in this script are present
+
 #load "CsvParser.fsx"
 
 open System
@@ -13,6 +15,7 @@ type MyRecord =
     }
 
 Path.Combine(__SOURCE_DIRECTORY__, "TestData.csv")
+|> FileInfo
 |> CsvParser.tryParse<MyRecord>
 |> Option.get
 |> Seq.iter (printfn "%A")
