026ea3c6937a1a85e88e102f140017a0517486c3
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-05-05 07:48:07 +0100

    Add failing test

--- TeqCrate.Test/TestUnion.fs
+++ TeqCrate.Test/TestUnion.fs
@@ -24,6 +24,18 @@ module TestUnion =
         | [<Bar>] Case3
         | [<Foo ; Bar>] Case4 of int64
 
+    let getAttributes field =
+        // The F# compiler puts in a bunch of attributes on each of these cases; we're not interested in that.
+        field.Attributes
+        |> List.filter (fun attr ->
+            [
+                typeof<Microsoft.FSharp.Core.CompilationMappingAttribute>
+                typeof<System.SerializableAttribute>
+                typeof<System.Diagnostics.DebuggerDisplayAttribute>
+            ]
+            |> List.forall (fun t -> attr.AttributeType <> t)
+        )
+
     [<Fact>]
     let ``Custom attributes are populated correctly for union cases`` () =
         let data =
@@ -40,19 +52,7 @@ module TestUnion =
         let attributes =
             fields
             |> List.map (fun field ->
-                // The F# compiler puts in a bunch of attributes on each of these cases; we're not interested in that.
-                let attributes =
-                    field.Attributes
-                    |> List.filter (fun attr ->
-                        [
-                            typeof<Microsoft.FSharp.Core.CompilationMappingAttribute>
-                            typeof<System.SerializableAttribute>
-                            typeof<System.Diagnostics.DebuggerDisplayAttribute>
-                        ]
-                        |> List.forall (fun t -> attr.AttributeType <> t)
-                    )
-
-                field.Name, attributes
+                field.Name, getAttributes field
             )
             |> Map.ofList
 
@@ -74,3 +74,35 @@ module TestUnion =
         |> List.map (fun data -> data.AttributeType)
         |> List.sortBy (fun ty -> ty.Name)
         |> shouldEqual [ typeof<Bar> ; typeof<Foo> ]
+
+    type PrivateFieldUnion =
+        private
+        | Field1
+        | [<Foo>] Field2
+
+    [<Fact>]
+    let ``Can obtain field data for private-implementation unions`` () =
+        let data =
+            match tType<PrivateFieldUnion> with
+            | Union data -> data
+            | _ -> failwith "Unexpected type"
+
+        let fields =
+            { new UnionConvEvaluator<PrivateFieldUnion, _> with
+                member _.Eval (fields : UnionTypeField list) _ _ = fields
+            }
+            |> data.Apply
+
+        let attributes =
+            fields
+            |> List.map (fun field ->
+                field.Name, getAttributes field
+            )
+            |> Map.ofList
+
+        attributes.Count |> shouldEqual 2
+        attributes.["Field1"] |> shouldBeEmpty
+        attributes.["Field2"]
+        |> List.exactlyOne
+        |> fun data -> data.AttributeType
+        |> shouldEqual typeof<Foo>
